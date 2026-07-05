812a7f381c00bdc06104b4fb54ee82851f6dde60
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-05 14:03:35 +0200

    feat: implement contact details component with dynamic rendering and styling

--- src/app/sections/contacts-detail/contacts-detail.ts
+++ src/app/sections/contacts-detail/contacts-detail.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-contacts-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacts-detail.html',
-  styleUrl: './contact-detail.scss',
+  styleUrls: ['./contacts-detail.scss']
 })
-export class ContactDetail {}
+export class ContactsDetailComponent {
+  @Input() public contact: any | null = null;
+
+  @Output() public edit = new EventEmitter<any>();
+  @Output() public delete = new EventEmitter<any>();
+
+  public onEditContact(): void {
+    if (this.contact) 
+      this.edit.emit(this.contact);
+  }
+
+  public onDeleteContact(): void {
+    if (this.contact) 
+      this.delete.emit(this.contact);
+  }
+}
