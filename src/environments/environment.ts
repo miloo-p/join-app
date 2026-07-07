9e4d9ebb605d5d1e0c2705e08a38fbe7a01e26b6
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-07-07 15:24:34 +0200

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { production: true, apiKey: 'sb_publishable_1zkb9XmlwE-t_X2CkBPvSA_l1-RXAqZ', apiUrl: 'https://raykjgrprjrcpbrozpxw.supabase.co' };
+export const environment = { production: true, apiKey: 'sb_publishable_MqEirnGHf2PsIfRALz31uA_hdBiQfiD', apiUrl: 'https://raykjgrprjrcpbrozpxw.supabase.co' };
