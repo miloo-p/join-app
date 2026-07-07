94ae39cc85a56e7577ec0332000240f5e6629e2e
Timo <timoboen@gmail.com>
2026-07-07 17:57:24 +0200

    feat: implement full header & responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout/sidebar/sidebar';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, Header],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
