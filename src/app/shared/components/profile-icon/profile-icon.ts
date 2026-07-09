ad2d6d41a328e92789c9d3f981de72ef3478e0ba
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-09 15:33:40 +0200

    feat: create new component profile-icon

---
+++ src/app/shared/components/profile-icon/profile-icon.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-icon',
+  imports: [],
+  templateUrl: './profile-icon.html',
+  styleUrl: './profile-icon.scss',
+})
+export class ProfileIcon {}
