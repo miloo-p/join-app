696196befaad09dc320faed35b88b4f4f698051c
Denis Nolting <denis.nolting@gmx.de>
2026-07-10 12:08:23 +0200

    feat: init main add task component and sub components

---
+++ src/app/pages/add-task/component/add-task-basic-info/add-task-basic-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-basic-info',
+  imports: [],
+  templateUrl: './add-task-basic-info.html',
+  styleUrl: './add-task-basic-info.scss',
+})
+export class AddTaskBasicInfo {}
---
+++ src/app/pages/add-task/component/add-task-detail-info/add-task-detail-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-detail-info',
+  imports: [],
+  templateUrl: './add-task-detail-info.html',
+  styleUrl: './add-task-detail-info.scss',
+})
+export class AddTaskDetailInfo {}
