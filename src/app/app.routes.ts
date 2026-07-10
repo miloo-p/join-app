8e4439a3cc13701644e26fd139ecf186f4fe0f5c
Denis Nolting <denis.nolting@gmx.de>
2026-07-10 12:24:37 +0200

    feat: connect mixins to scss and subcomponents to maincomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Contacts } from './pages/contacts/contacts';
 import { MainPage } from './pages/main-page/main-page';
+import { AddTask } from './pages/add-task/add-task';
 
 export const routes: Routes = [
     { path: '', component: MainPage },
-    { path: 'contacts', component: Contacts }
+    { path: 'contacts', component: Contacts },
+    { path: 'add-task', component: AddTask },
 ];
--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AddTaskBasicInfo } from './component/add-task-basic-info/add-task-basic-info';
+import { AddTaskDetailInfo } from './component/add-task-detail-info/add-task-detail-info';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [AddTaskBasicInfo, AddTaskDetailInfo],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
