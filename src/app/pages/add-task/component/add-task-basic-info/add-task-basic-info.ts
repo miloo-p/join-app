02e3fa88d7c2767aca596f9dae849255b328973c
Denis Nolting <denis.nolting@gmx.de>
2026-07-10 18:32:53 +0200

    feat: general html and scss

--- src/app/pages/add-task/component/add-task-basic-info/add-task-basic-info.ts
+++ src/app/pages/add-task/component/add-task-basic-info/add-task-basic-info.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task-basic-info',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task-basic-info.html',
   styleUrl: './add-task-basic-info.scss',
 })
-export class AddTaskBasicInfo {}
+export class AddTaskBasicInfo {
+  today = new Date().toISOString().split('T')[0];
+  dueDate = '';
+
+  formatDateForDisplay(date: string): string {
+    const [year, month, day] = date.split('-');
+    return `${day}/${month}/${year}`;
+  }
+}
--- src/app/pages/add-task/component/add-task-detail-info/add-task-detail-info.ts
+++ src/app/pages/add-task/component/add-task-detail-info/add-task-detail-info.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-add-task-detail-info',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './add-task-detail-info.html',
   styleUrl: './add-task-detail-info.scss',
 })
