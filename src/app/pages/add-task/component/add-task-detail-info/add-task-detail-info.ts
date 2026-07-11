dbebbd84b50af1f9aa88dec582d145021d1b690c
Denis Nolting <denis.nolting@gmx.de>
2026-07-11 11:37:19 +0200

    feat: add contactService + for-loop to select user in form

--- src/app/pages/add-task/component/add-task-detail-info/add-task-detail-info.ts
+++ src/app/pages/add-task/component/add-task-detail-info/add-task-detail-info.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { Supabase } from '../../../../shared/services/supabase';
 
 @Component({
   selector: 'app-add-task-detail-info',
+  standalone: true,
   imports: [ButtonComponent],
   templateUrl: './add-task-detail-info.html',
   styleUrl: './add-task-detail-info.scss',
 })
-export class AddTaskDetailInfo {}
+export class AddTaskDetailInfo implements OnInit{
+  contactDatabase = inject(Supabase);
+
+  async ngOnInit() {
+    await this.contactDatabase.getContacts();
+    await this.contactDatabase.subscribeToContacts();
+  }
+}
