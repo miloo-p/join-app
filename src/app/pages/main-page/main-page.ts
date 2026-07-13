007d9f0d3a4bc2c272d4219a7c6409e04cd7f6b1
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-07-13 13:03:20 +0200

    feat add: TasksService

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { contactsService } from '../../shared/services/contacts-service';
+import { tasksService } from '../../shared/services/tasks-service';
 import { Contacts } from '../contacts/contacts';
 
 @Component({
@@ -10,10 +11,24 @@ import { Contacts } from '../contacts/contacts';
 })
 export class MainPage {
 
-  dbContacts = inject(contactsService)
+  dbContacts = inject(contactsService);
+  dbTasks = inject(tasksService);
 
   ngOnInit() {
     this.dbContacts.getContacts();
     this.dbContacts.subscribeToContacts();
+
+    this.dbTasks.getTasks();
+    this.dbTasks.subscribeToTasks();
+    // this.dbTasks.setTask([{
+    //   title: 'Test Task',
+    //   desc: 'This is a test task',
+    //   due_date: '2024-06-30',
+    //   status: 1,
+    //   priority: 1,
+    //   collaborators: [3,4] ,
+    //   subtasks: [{name: 'Subtask 1', status: 0}, {name: 'Subtask 2', status: 0}]
+    // }]);
   }
+  
 }
