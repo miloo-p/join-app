813bc4b53275ec9ef5e9c8a327d3bc1eb338402a
Denis Nolting <denis.nolting@gmx.de>
2026-07-13 12:17:05 +0200

    feat: openDatePicker method for click on date input field

--- src/app/pages/add-task/component/add-task-basic-info/add-task-basic-info.ts
+++ src/app/pages/add-task/component/add-task-basic-info/add-task-basic-info.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task-basic-info',
-  imports: [FormsModule],
+  imports: [FormsModule,],
   templateUrl: './add-task-basic-info.html',
   styleUrl: './add-task-basic-info.scss',
 })
@@ -14,5 +14,9 @@ export class AddTaskBasicInfo {
   formatDateForDisplay(date: string): string {
     const [year, month, day] = date.split('-');
     return `${day}/${month}/${year}`;
+  }
+
+  openDatePicker(input: HTMLInputElement): void {
+    input.showPicker();
   }
 }
