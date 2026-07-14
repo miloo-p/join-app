861980e516a35879af238a0e1d4e9c2d2aa2e7a1
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-14 20:19:17 +0200

    component contact-success-message

---
+++ src/app/shared/components/contact-success-message/contact-success-message.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-success-message',
+  imports: [],
+  templateUrl: './contact-success-message.html',
+  styleUrl: './contact-success-message.scss',
+})
+export class ContactSuccessMessage {}
