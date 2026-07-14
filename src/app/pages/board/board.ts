5816f410fe7747b3e35c40ed897e3eb39f09d0af
Timo <timoboen@gmail.com>
2026-07-14 10:50:04 +0200

    feat: add component task-overlay

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskCard } from './components/task-card/task-card';
+import { TaskOverlay } from './components/task-overlay/task-overlay';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [TaskCard],
+  imports: [TaskCard, TaskOverlay],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
---
+++ src/app/pages/board/components/task-overlay/task-overlay.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-overlay',
+  imports: [],
+  templateUrl: './task-overlay.html',
+  styleUrl: './task-overlay.scss',
+})
+export class TaskOverlay {}
