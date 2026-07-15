d3360f6231e2ef40f9ac1a1eef589760bc2469e2
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-15 15:33:28 +0200

    changes

--- src/app/shared/components/contact-success-message/contact-success-message.ts
+++ src/app/shared/components/contact-success-message/contact-success-message.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+/* input holen von unten von contact-add-new-contact-dialog-html , sonst kann man den Test nicht lesen!Verküpfung*/
+
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-success-message',
@@ -6,4 +9,8 @@ import { Component } from '@angular/core';
   templateUrl: './contact-success-message.html',
   styleUrl: './contact-success-message.scss',
 })
-export class ContactSuccessMessage {}
+export class ContactSuccessMessage {
+
+  @Input() message = '';
+
+}
