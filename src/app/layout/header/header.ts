60b482217362fb56cb318c334fbb45df7402f7ee
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-16 12:07:47 +0200

    test profile icon Magdalena in violett

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LogoDark } from '../../shared/components/logo-dark/logo-dark';
+import { ProfileIcon } from '../../shared/components/profile-icon/profile-icon';
+
 
 
 @Component({
   selector: 'app-header',
-  imports: [LogoDark],
+  imports: [LogoDark, ProfileIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
