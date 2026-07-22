c2306e1a3ef753356747d6ceb87911a082748575
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-07-22 15:37:49 +0200

    oninit @task-card.ts

--- src/app/pages/board/components/task-card/task-card.ts
+++ src/app/pages/board/components/task-card/task-card.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Task } from '../../../../shared/interfaces/tasks';
 import { tasksService } from '../../../../shared/services/tasks-service';
 
