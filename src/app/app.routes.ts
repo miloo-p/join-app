47ac7b67ac38ffeffd28b545250b6c5c5172c7ca
Denis Nolting <denis.nolting@gmx.de>
2026-07-23 16:44:21 +0200

    feat: add summary comp and connect to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Contacts } from './pages/contacts/contacts';
 import { Board } from './pages/board/board';
 import { AddTask } from './pages/add-task/add-task';
+import { Summary } from './pages/summary/summary';
 
 export const routes: Routes = [
+  { path: '', component: Summary},
   { path: 'contacts', component: Contacts },
   { path: 'board', component: Board },
-    { path: 'contacts', component: Contacts },
-    { path: 'add-task', component: AddTask },
+  { path: 'contacts', component: Contacts },
+  { path: 'add-task', component: AddTask },
 ];
