52b113fba26d5d7fcdbe1651d46f78765ee78378
Denis Nolting <denis.nolting@gmx.de>
2026-07-24 15:17:00 +0200

    feat: get closest date from urgent task

--- src/app/pages/summary/summary.ts
+++ src/app/pages/summary/summary.ts
@@ -1,11 +1,14 @@
 import { Component, computed, inject, OnInit } from '@angular/core';
 import { tasksService } from '../../shared/services/tasks-service';
+import { RouterLink } from '@angular/router';
+import { Task } from '../../shared/interfaces/tasks';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
   templateUrl: './summary.html',
   styleUrl: './summary.scss',
+  imports: [RouterLink],
 })
 export class Summary implements OnInit {
   private tasksService = inject(tasksService);
@@ -30,8 +33,44 @@ export class Summary implements OnInit {
     return this.todoTasksCount() + this.progressTasksCount() + this.feedbackTasksCount();
   });
 
+  urgentTaskCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.priority === 2).length
+  })
+
+  closestDueTask = computed(() => {
+    const today = new Date().getTime();
+
+    return this.tasksService.tasks()
+      .filter(task => task.due_date)
+      .filter(task => new Date(task.due_date).getTime() >= today)
+      .reduce((closestTask, currentTask) => {
+        if (!closestTask) {
+          return currentTask;
+        }
+
+        const closestTime = new Date(closestTask.due_date).getTime();
+        const currentTime = new Date(currentTask.due_date).getTime();
+
+        return currentTime < closestTime ? currentTask : closestTask;
+      }, null as Task | null);
+  });
+
+  closestDueDate = computed(() => {
+    return this.closestDueTask()?.due_date ?? '';
+  });
+
   async ngOnInit(): Promise<void> {
     await this.tasksService.getTasks();
     await this.tasksService.subscribeToTasks();
   }
+
+  formatDateForSummary(date: string): string {
+  const parsedDate = new Date(date);
+
+  return parsedDate.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+}
 }
