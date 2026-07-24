d4dbfee6b1ff084df2747b335ba1eca80cd1f065
Denis Nolting <denis.nolting@gmx.de>
2026-07-24 13:40:59 +0200

    feat: add taskService & add methods for summary data

--- src/app/pages/summary/summary.ts
+++ src/app/pages/summary/summary.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { tasksService } from '../../shared/services/tasks-service';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  standalone: true,
   templateUrl: './summary.html',
   styleUrl: './summary.scss',
 })
-export class Summary {}
+export class Summary implements OnInit {
+  private tasksService = inject(tasksService);
+
+  todoTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 0).length;
+  });
+
+  progressTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 1).length;
+  });
+
+  feedbackTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 2).length;
+  });
+
+  doneTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 3).length;
+  });
+
+  tasksInBoard = computed(() => {
+    return this.todoTasksCount() + this.progressTasksCount() + this.feedbackTasksCount();
+  });
+
+  async ngOnInit(): Promise<void> {
+    await this.tasksService.getTasks();
+    await this.tasksService.subscribeToTasks();
+  }
+}
