1c20b6da15616b771604c6920ae66cb77618bcc4
Denis Nolting <denis.nolting@gmx.de>
2026-07-24 15:30:15 +0200

    feat: dynamic text depending on daytime

--- src/app/pages/summary/greeting/greeting.ts
+++ src/app/pages/summary/greeting/greeting.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './greeting.html',
   styleUrl: './greeting.scss',
 })
-export class Greeting {}
+export class Greeting {
+
+  get greetingGetter(): string {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 11) return 'Good morning!';
+    if (hour >= 11 && hour < 17) return 'Good day!';
+    if (hour >= 17 && hour < 22) return 'Good evening!';
+    return 'Good night!';
+  }
+}
